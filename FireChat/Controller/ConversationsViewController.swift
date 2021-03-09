7651a3b8eff6c5a23828e0bf66a8e03be4fec254
Aaron Cleveland <toscleveland@gmail.com>
2021-03-08 22:21:31 -0600

    Setup TableView

--- FireChat/Controller/ConversationsViewController.swift
+++ FireChat/Controller/ConversationsViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class ConversationsViewController: UIViewController {
+    
+    private let tableView = UITableView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,7 +20,7 @@ class ConversationsViewController: UIViewController {
     func configureUI() {
         view.backgroundColor = .white
         configureNavigationBar()
-        
+        configureTableView()
         let image = UIImage(systemName: "person.circle.fill")
         navigationItem.leftBarButtonItem = UIBarButtonItem(image: image,
                                                            style: .plain,
@@ -40,11 +42,42 @@ class ConversationsViewController: UIViewController {
         navigationController?.navigationBar.tintColor = .white
         navigationController?.navigationBar.isTranslucent = true
         
-        // TODO: - This is not changing status bar *figure out why* - 
+        // TODO: - This is not changing status bar *figure out why* -
         navigationController?.navigationBar.overrideUserInterfaceStyle = .dark
+    }
+    
+    func configureTableView() {
+        tableView.backgroundColor = .white
+        tableView.rowHeight = 80
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        // Allows us to remove empty rows that are needed
+        tableView.tableFooterView = UIView()
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        view.addSubview(tableView)
+        tableView.frame = view.frame
     }
     
     // MARK: - Actions -
     @objc func showProfile() {}
+    
+}
+
+extension ConversationsViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Test Cell"
+        cell.backgroundColor = .systemTeal
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath.row)
+    }
     
 }
